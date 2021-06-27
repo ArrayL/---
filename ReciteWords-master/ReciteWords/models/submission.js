dfcbe285b3c29e05861271022bfc44578f203775
yubowen <1985233591@qq.com>
2021-06-27 17:36:45 +0800

    最终项目提交

--- ReciteWords-master/ReciteWords/models/submission.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const Submission = new mongoose.Schema({
-    username: String,
-    unit: String
-}, { timestamps: true });
-
-module.exports = mongoose.model("submissions", Submission);
